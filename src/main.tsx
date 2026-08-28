17cde81e34b992a2a194fb0099f9b9af801cac4f
khoryik96-creator <khoryik96@gmail.com>
2026-08-28 09:07:45 +0800

    Load React reader styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import { ReaderProvider } from './features/reader/ReaderContext';
 import './styles/global.css';
+import './styles/reader.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element was not found.');
