017ef7bce8d9ac462ab9e97ac894de15f501fbac
khoryik96-creator <khoryik96@gmail.com>
2026-08-28 09:05:36 +0800

    Provide shared reader state to React app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
+import { ReaderProvider } from './features/reader/ReaderContext';
 import './styles/global.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element was not found.');
 
-createRoot(root).render(<StrictMode><App /></StrictMode>);
+createRoot(root).render(
+  <StrictMode>
+    <ReaderProvider>
+      <App />
+    </ReaderProvider>
+  </StrictMode>,
+);
